9e60404ac4c71d25be6a94ae974b1da5a88a7c9c
Jungdo Lee <lee.jungdo@gmail.com>
2019-02-07 16:20:08 +0900

    Finish project

--- src/App.js
+++ src/App.js
@@ -2,8 +2,77 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    password: '',
+    confirmPassword: ''
+  };
+
+  handleOnPasswordInput(passwordInput) {
+    this.setState({ password: passwordInput });
+  }
+
+  handleOnConfirmPasswordInput(confirmPasswordInput) {
+    this.setState({ confirmPassword: confirmPasswordInput });
+  }
+
+  doesPasswordMatch() {
+    const { password, confirmPassword } = this.state;
+    return password === confirmPassword;
+  }
+
+  confirmPasswordClassName() {
+    const { confirmPassword } = this.state;
+
+    if (confirmPassword) {
+      return this.doesPasswordMatch() ? 'is-valid' : 'is-invalid';
+    }
+  }
+
+  renderFeedbackMessage() {
+    const { confirmPassword } = this.state;
+
+    if (confirmPassword) {
+      if (!this.doesPasswordMatch()) {
+        return (
+          <div className="invalid-feedback">패스워드가 일치하지 않습니다</div>
+        );
+      }
+    }
+  }
+
   render() {
-    return <div className="App">Hi</div>;
+    return (
+      <div className="App">
+        <form className="my-form">
+          <div className="form-row">
+            <div className="col-md-12 mb-3">
+              <label htmlFor="passwordInput">패스워드</label>
+              <input
+                type="password"
+                className="form-control"
+                id="passwordInput"
+                onChange={e => this.handleOnPasswordInput(e.target.value)}
+              />
+            </div>
+            <div className="col-md-12 mb-3">
+              <label htmlFor="confirmPasswordInput">패스워드 확인</label>
+              <input
+                type="password"
+                className={`form-control ${this.confirmPasswordClassName()}`}
+                id="confirmPasswordInput"
+                onChange={e =>
+                  this.handleOnConfirmPasswordInput(e.target.value)
+                }
+              />
+              {this.renderFeedbackMessage()}
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary btn-block">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
